47c1d188244c60a1cc3ab136192d40eef63c9e17
Cruise Brantley <cruise.brantley@gmail.com>
2019-04-08 20:29:26 -0500

    Complete refactor and design, working corner boxes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		grow: 0,
+		selected: 0
+	};
+	render() {
+		return (
+			<div className="App">
+				<div
+					className={this.evalClass(1)}
+					onMouseEnter={() => this.mouseEnterCorner(1)}
+					onMouseDown={() => this.mouseClickCorner(1)}
+					onMouseLeave={this.mouseLeaveCorner}
+				/>
+				<div
+					className={this.evalClass(2)}
+					onMouseEnter={() => this.mouseEnterCorner(2)}
+					onMouseDown={() => this.mouseClickCorner(2)}
+					onMouseLeave={this.mouseLeaveCorner}
+				/>
+				<div
+					className={this.evalClass(3)}
+					onMouseEnter={() => this.mouseEnterCorner(3)}
+					onMouseDown={() => this.mouseClickCorner(3)}
+					onMouseLeave={this.mouseLeaveCorner}
+				/>
+				<div
+					className={this.evalClass(4)}
+					onMouseEnter={() => this.mouseEnterCorner(4)}
+					onMouseDown={() => this.mouseClickCorner(4)}
+					onMouseLeave={this.mouseLeaveCorner}
+				/>
+			</div>
+		);
+	}
+
+	evalClass = num => {
+		if (this.state.selected === num) return `corner${num} selected`;
+		if (this.state.grow === num) return `corner${num} grow`;
+		return `corner${num} shrink`;
+	};
+
+	mouseEnterCorner = num => {
+		this.setState({ grow: num });
+	};
+	mouseLeaveCorner = () => {
+		this.setState({ grow: 0 });
+	};
+	mouseClickCorner = num => {
+		this.setState({ selected: num });
+	};
 }
 
 export default App;
