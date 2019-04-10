6b854b2b4589d7c1c4c41ea26fb3e2b5bcc4615c
Cruise Brantley <cruise.brantley@gmail.com>
2019-04-10 15:56:30 -0500

    Matching deselect pattern with outer-square

--- src/components/corner-full.js
+++ src/components/corner-full.js
@@ -6,9 +6,6 @@ class Corner extends Component {
 			<div>
 				<h1>{this.props.subject}</h1>
 				<h3>This is where the subject information would continue.</h3>
-				<div>
-					<button onClick={this.props.deselect}>Shrink</button>
-				</div>
 			</div>
 		);
 	}
