398b0f6f27fbac5dddf2f89a9b3b471f31d43438
Cruise Brantley <cruise.brantley@gmail.com>
2019-04-12 18:45:29 -0500

    Created Bio WIP

---
+++ src/components/bio.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Bio extends Component {
+	render() {
+		return (
+			<div className="corner-full-container">
+				<div className="bio-container">
+					<h1>About Cruise</h1>
+
+					<p>
+						Full Stack Web Developer who is passionate about new technologies
+						and the user experience. Background of ten years in customer service
+						and experience breaking down technical issues into understandable
+						language. Enjoys feedback and endeavors to improve. Has the passion
+						required to keep current with developments and ready to acquire new
+						skills.
+					</p>
+				</div>
+			</div>
+		);
+	}
+}
+export default Bio;
