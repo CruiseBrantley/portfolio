f9bb9e2fc0dd3f20c75711bedb94214d8732953f
Cruise Brantley <cruise.brantley@gmail.com>
2019-04-15 19:30:26 -0500

    Created education page

---
+++ src/components/education.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class Education extends Component {
+	render() {
+		return (
+			<div className="corner-full-container">
+				<div className="education-container">
+					<h1>Education</h1>
+					<h3>Lambda School</h3>
+					<h4>Web Development and Computer Science</h4>
+					<h4>2018 - 2019</h4>
+					<span>
+						Lambda School is a 6+ month Computer Science & Software Engineering
+						Academy that provides an immersive hands-on curriculum with a focus
+						on computer science, software engineering, and web development.
+					</span>
+					<li>
+						Utilized agile software development and Git workflow on all
+						projects.
+					</li>
+					<li>Gained hands-on experience with client and server testing.</li>
+					<li>
+						Completed all curriculum course work including: React, Redux, Node,
+						Express, MongoDB, Jest, Python, Django, etc.
+					</li>
+					<li>
+						Wrote production-ready code using ReactJS, Redux, and CSS on the
+						front-end and NodeJS and Express on the back-end to build single
+						page applications.
+					</li>
+					<li>
+						Mastered five-week supplemental course on back-end web development
+						using Java and Spring Boot.
+					</li>
+					<h3>Henderson State University</h3>
+					<h4>Computer Science</h4>
+					<h4>2008 - 2011</h4>
+					<span>
+						Completed 67 credit hours towards BS in Computer Science, including
+						these pertinent classes.
+					</span>
+					<li>Computer Organization</li>
+					<li>Assembler Language</li>
+					<li>Data Structures</li>
+					<li>Foundations of Computer Science I and II</li>
+				</div>
+			</div>
+		);
+	}
+}
+export default Education;
