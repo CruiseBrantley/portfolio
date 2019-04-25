12117d7b96f94b8e8bba31277e614e0bcaf08474
Cruise Brantley <cruise.brantley@gmail.com>
2019-04-24 22:12:25 -0500

    Added image code and CSS to support

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,28 +2,52 @@ import React, { Component } from "react";
 import programs from "./programs.json";
 
 class Projects extends Component {
+	importAll(r) {
+		let images = {};
+		r.keys().forEach((item, index) => {
+			images[item.replace("./", "")] = r(item);
+		});
+		return images;
+	}
+
 	render() {
+		const images = this.importAll(
+			require.context("../images", false, /\.(png|jpe?g|svg)$/)
+		);
 		return (
 			<div className="corner-full-container">
 				<div className="projects-container">
 					<h1>Projects</h1>
 					{programs.map((program, index) => {
 						return (
-							<div key={index}>
-								<h3>{program.Title}</h3>
-								<br />
-								{program.DescriptionArr.map((description, index) => {
-									return (
-										<>
-											<span key={index}>{description}</span>
-											<br />
-										</>
-									);
-								})}
-								<li>{program.Span}</li>
-								{program.Contributions ? (
-									<li>{program.Contributions}</li>
-								) : null}
+							<div
+								className={
+									index % 2 === 0 ? "project-div image-left" : "project-div"
+								}
+								key={index}
+							>
+								<div className="project-text">
+									<h3>{program.Title}</h3>
+									<a href={program.Link}>{program.LinkText}</a>
+									<br />
+									{program.DescriptionArr.map((description, index) => {
+										return (
+											<div key={index}>
+												<span key={index}>{description}</span>
+												<br />
+											</div>
+										);
+									})}
+									<li>{program.Span}</li>
+									{program.Contributions ? (
+										<li>{program.Contributions}</li>
+									) : null}
+								</div>
+								<img
+									alt=""
+									className="project-image"
+									src={images[program.Image]}
+								/>
 							</div>
 						);
 					})}
